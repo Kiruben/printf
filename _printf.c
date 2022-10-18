11c4b7fd9f7f07de0a9eeddeafb424d1888db8ee
Kiruben <kiruben.dev@gmail.com>
2022-10-18 06:02:16 -0700

    initialising files

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ *_printf - prototype function
+ *Description: function for format printing
+ *@format: list of arguments to print
+ *Return: Number of printed characters
+ */
+int _print(const char *format, ...)
+{
+  return(1);
+}
